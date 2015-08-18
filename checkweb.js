c426d87b5d1513403b713a2a1425ce581ed2afb7
gaudinat <arnaud.gaudinat@hesge.ch>
2015-08-18 09:50:25 +0200

    reorganise to separate all cfg

---
+++ checkweb.js
@@ -0,0 +1,4 @@
+var checkweb = require ('./lib/checkweb.js');
+
+// load the services to check from the cfg files
+checkweb.loadServices();
--- test/checkweb-test.js
+++ test/checkweb-test.js
@@ -1,1 +1,19 @@
-var checkweb require = ('../lib/checkweb.js');
+var cfg         = require('./cfg.json');
+var webGoogle   = require('./services/web-google.json');
+var checkweb    = require('../lib/checkweb.js');
+var assert      = require('assert');
+
+
+// test testContent
+describe('checkweb', function(){
+  describe('.testWebService(web-google)', function(){
+    it('should test the result of a web service', function(){
+      checkweb.testWebService(webGoogle,function(error,data){
+        if (error) {
+          console.log(error);
+        }
+        assert(data === true);
+      });
+    });
+  });
+});
